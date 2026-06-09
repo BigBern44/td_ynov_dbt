73fe89f7414747dff6a0c8d555771595d6b469f5
eliott.bernard@ynov.com <eliott.bernard@ynov.com>
2026-06-09 12:16:59 +0000

    feature add account_manager_region stage

---
+++ models/staging/google_sheets/stg_account_manager_region_mapping.sql
@@ -0,0 +1,41 @@
+with
+
+source as (
+
+    select * from {{ source('google_sheets', 'account_manager_region_mapping') }}
+
+),
+
+renamed as (
+
+    select
+        -- pas de clé primaire métier : on en crée une (un account manager par État)
+        {{ dbt_utils.generate_surrogate_key(['State']) }} as region_mapping_id,
+
+        -- colonnes métier (renommées en snake_case)
+        State                                             as state,
+        Account_Manager                                   as account_manager,
+
+        -- date de chargement Airbyte conservée (utile pour la fraîcheur / le suivi)
+        _airbyte_extracted_at                             as loaded_at
+
+        -- colonnes techniques Airbyte ignorées :
+        -- _airbyte_raw_id, _airbyte_meta, _airbyte_generation_id
+
+    from source
+
+),
+
+deduplicated as (
+
+    -- une seule ligne par État ; en cas de doublon, on garde le chargement le plus récent
+    select *
+    from renamed
+    qualify row_number() over (
+        partition by region_mapping_id
+        order by loaded_at desc
+    ) = 1
+
+)
+
+select * from deduplicated
--- models/staging/sales_database/stg_sales_database_user.sql
+++ models/staging/sales_database/stg_sales_database_user.sql
@@ -2,23 +2,34 @@ with
 
 source as (
 
-    select * from {{ source('sales_database', 'seller') }}
+    select * from {{ source('sales_database', 'user') }}
 
 ),
 
 renamed as (
 
     select
-        -- clé primaire
-        seller_id,
+        -- "user_name" est en réalité l'identifiant client -> on le renomme
+        user_name                       as customer_id,
 
-        -- attributs ; le code postal reste en string (peut contenir des zéros à gauche)
-        cast(seller_zip_code as string) as seller_zip_code,
-        seller_city,
-        seller_state
+        -- attributs ; le code postal reste en string
+        cast(customer_zip_code as string) as customer_zip_code,
+        customer_city,
+        customer_state
+
+        -- la colonne "row_num" est un artefact technique : on ne la garde pas
 
     from source
 
+),
+
+deduplicated as (
+
+    -- suppression des éventuels doublons sur la clé client
+    select *
+    from renamed
+    qualify row_number() over (partition by customer_id order by customer_id) = 1
+
 )
 
-select * from renamed
+select * from deduplicated
